b9fd97842e71590ca2c524a351f6bc828c7c7067
GabrielGodin02 <gabrielgodin02@gmail.com>
2023-02-13 17:14:41 +0300

    cambio2

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import {Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -12,6 +12,9 @@ const Login = () => {
             name="correo"
             placeholder="Digite Su Correo Eletronico"
           ></input>
+        </div>
+        <div>
+          <label>Contraseña</label>
           <input
             type="password"
             id="contraseña"
@@ -19,6 +22,7 @@ const Login = () => {
             placeholder="digite su contraseña"
           ></input>
         </div>
+        <a href="#">No estas Registrado</a>
       </form>
     </>
   );
